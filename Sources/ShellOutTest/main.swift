37070ff4097cc8a43ac56be3d396de60d538a6eb
Vladislav <rct.stankevivs@gmail.com>
2021-03-25 23:37:00 +0300

    scriptCommit3

--- Sources/ShellOutTest/main.swift
+++ Sources/ShellOutTest/main.swift
@@ -2,7 +2,7 @@ import ShellOut
 import ArgumentParser
 
 struct Command: ParsableCommand {
-    static let configuration = CommandConfiguration(abstract: "Вызов коммнды", subcommands: [Push.self, Createbrunch.self])
+    static let configuration = CommandConfiguration(abstract: "Вызов команды", subcommands: [Push.self, Createbrunch.self, Gitinit.self, Openfile.self, Createandopenfile.self])
 }
 
 extension Command {
@@ -26,6 +26,25 @@ extension Command {
         
         func run() throws {
             try shellOut(to: "git checkout -b \(branchName)")
+        }
+    }
+    
+    struct Gitinit: ParsableCommand {
+        func run() throws {
+            try shellOut(to: .gitInit())
+        }
+    }
+    
+    struct Openfile: ParsableCommand {
+        func run() throws {
+            try shellOut(to: .openFile(at: "README.md"))
+        }
+    }
+    
+    struct Createandopenfile: ParsableCommand {
+        func run() throws {
+            try shellOut(to: .createFile(named: "file", contents: "Hello world"))
+            try shellOut(to: .openFile(at: "file"))
         }
     }
 }
